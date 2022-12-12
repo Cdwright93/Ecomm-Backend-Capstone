612e3e52bf7220d7b7cdf5db188523c9f50b5e89
Clayton  Wright <81819518+Cdwright93@users.noreply.github.com>
2022-12-12 09:57:35 -0500

    mvp operational ironing quirks

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 var { mongoConnect } = require("./mongo.js");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 mongoConnect();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
+
 
 var app = express();
 
@@ -30,6 +32,32 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.post("/create-checkout-session", async (req, res) => {
+	try {
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ["card"],
+			line_items: [
+				{
+					price_data: {
+						currency: "usd",
+						product_data: {
+							name: req.body.name,
+						},
+						unit_amount: req.body.price * 100,
+					},
+					quantity: 1,
+				},
+			],
+			mode: "payment",
+			success_url: "http://localhost:3000/",
+			cancel_url: "http://localhost:4000/cancel",
+		})
+		res.json({ url: session.url });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 // app.use("/blogs", blogsRouter);
 
 // catch 404 and forward to error handler
